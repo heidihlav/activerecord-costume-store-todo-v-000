7e0cacdee06ddf21a814aaa684b73a4723252999
Heidi Hlavinka <hchlav@gmail.com>
2018-11-14 02:14:44 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,7 +5,7 @@ class HauntedHouse < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :location
       t.string :theme
-      t.integer :num_of_employees
+      t.integer :price
       t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
